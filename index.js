bbee099057d6617f0cf600dc4db6ceaa7a873507
Íñigo Paños <inigopanos@gmail.com>
2022-11-13 19:19:49 +0100

    En app.use access-control...

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-  //res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
