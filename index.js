5c392c003f78e50676e9e6a2ddd888173381bdfd
Íñigo Paños <inigopanos@gmail.com>
2022-11-13 19:22:46 +0100

    Quitado app.use, puesto origin a micaapp.netlify/tabs/tab1

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = 4000;
 app.use(express.json());
 //'https://micapp.netlify.app'
 var corsOptions = {
-  origin: '*',
+  origin: 'https://micapp.netlify.app/tabs/tab1',
   methods: ['GET', 'POST'],
   origin: true,
   credentials: true,
@@ -23,7 +23,7 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  //res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
