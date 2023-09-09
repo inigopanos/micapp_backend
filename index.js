60827f530b2a4456c46c9a352061d0e4e4e8b351
Iñigo <inigopanos@gmail.com>
2023-09-09 16:39:36 +0200

    Añadido app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000})); para intentar solventar el problema de muchos datos

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ dotenv.config();
 export const app = express();
 const PORT = process.env.PORT;
 
+//  https://stackoverflow.com/questions/50304779/payloadtoolargeerror-request-entity-too-large
+
 // app.use(express.json());
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
+
 const corsOptions = {
   origin: 'http://localhost',
   methods: ['GET', 'POST'],
